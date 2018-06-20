5b0df1fa41463283b586e78e89543e34045061ef
Kevin Jang <kevin.a.jang@gmail.com>
2018-06-20 16:05:21 +1000

    Controller now has access to the main method

--- oztamautomation-javafx/src/main/java/com/oztamautomation/jsonfinder/JSONFinderController.java
+++ oztamautomation-javafx/src/main/java/com/oztamautomation/jsonfinder/JSONFinderController.java
@@ -1,5 +1,30 @@
 package com.oztamautomation.jsonfinder;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import com.oztamautomation.jsonfinder.JSONFinderMain;
+
 public class JSONFinderController {
+	
+	// Create an instance of the main class
+	private JSONFinderMain jsonFinderMain;
+	
+	public void setMain(JSONFinderMain jsonFinderMain) {
+		// Get the instance of the main class object
+		this.jsonFinderMain = jsonFinderMain;
+	}
+	
+	
+	
+//	@FXML
+//	private Button buttonGetJSONLogs;
+//	public void createJSONLog(ActionEvent event) {
+//		
+//		JSONFinderMain.main(args);
+//		
+//		
+//		
+//	}
 	
 }
--- oztamautomation-javafx/src/main/java/com/oztamautomation/jsonfinder/JSONFinderMain.java
+++ oztamautomation-javafx/src/main/java/com/oztamautomation/jsonfinder/JSONFinderMain.java
@@ -8,14 +8,34 @@ import javafx.fxml.FXMLLoader;
 
 
 public class JSONFinderMain extends Application {
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("JSONFinderView.fxml"));
-			Scene scene = new Scene(root,1300,750);
+//			// Create the main anchor pane from the FXML file
+//			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("JSONFinderView.fxml"));
+			
+			// Create a FXML file loader object from the JSONFinderView.fxml file
+			FXMLLoader fxmlFileLoader = new FXMLLoader(JSONFinderMain.class.getResource("JSONFinderView.fxml"));
+			// Create the main GUI anchor pane and load the FXML objects onto it
+			AnchorPane anchorPane = fxmlFileLoader.load();
+
+			// Create an instance of the Controller from the FXML controller
+			JSONFinderController jsonFinderController = fxmlFileLoader.getController();
+			jsonFinderController.setMain(this);
+			
+			// Create the main container for all contents in the Scene graph
+			Scene scene = new Scene(anchorPane,1300,750);
+			
+			// Apply the CSS
 			scene.getStylesheets().add(getClass().getResource("JSONFinder.css").toExternalForm());
+			
+			// Specify the scene to be used on the Stage
 			primaryStage.setScene(scene);
+			
+			// Show the GUI
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
