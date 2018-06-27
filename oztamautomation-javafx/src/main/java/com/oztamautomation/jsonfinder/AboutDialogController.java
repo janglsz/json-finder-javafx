22a7e29b36eaa6354d350044477508c578b19b07
Kevin Jang <kevin.a.jang@gmail.com>
2018-06-27 16:48:14 +1000

    Partial Implemention of AboutDialog MVC code

---
+++ oztamautomation-javafx/src/main/java/com/oztamautomation/jsonfinder/AboutDialogController.java
@@ -0,0 +1,11 @@
+package com.oztamautomation.jsonfinder;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class AboutDialogController {
+	
+	@FXML
+	TextArea aboutText;
+
+}
