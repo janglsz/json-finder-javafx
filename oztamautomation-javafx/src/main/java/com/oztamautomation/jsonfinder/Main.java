fd5a2187f4042e2f885a2e5114717e6632e2fb97
Kevin Jang <kevin.a.jang@gmail.com>
2018-08-01 21:25:24 +1000

    Refactors code

--- oztamautomation-javafx/src/main/java/com/oztamautomation/jsonfinder/Main.java
+++ oztamautomation-javafx/src/main/java/com/oztamautomation/jsonfinder/Main.java
@@ -346,8 +346,8 @@ public class Main extends Application {
 	public Main() {
 		
 		// INITIALISE VARIABLES
-		setUsername("oztam");
-		setPassword("IFY27aLfU6Rhkoo");
+		setUsername("");
+		setPassword("");
 		setCurrentSessionId("");
 		
 		// INITIALISE OBJECTS
